7bb6dd05e0f1978042960162c411100471ded2e7
shagie <turnmichael@gmail.com>
2022-03-28 20:00:49 -0500

    suppress

--- src/main/java/net/shagie/tasks/Sorter.java
+++ src/main/java/net/shagie/tasks/Sorter.java
@@ -10,6 +10,7 @@ import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 
+@SuppressWarnings("java:S106")
 public class Sorter {
     private static final Logger LOG = LoggerFactory.getLogger(Sorter.class);
 
