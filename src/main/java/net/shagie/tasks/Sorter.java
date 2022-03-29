a9f91c0501391882ccc03fb27d0ee28bc017123f
shagie <turnmichael@gmail.com>
2022-03-28 19:54:22 -0500

    feat: Logging and exception handling

--- src/main/java/net/shagie/tasks/Sorter.java
+++ src/main/java/net/shagie/tasks/Sorter.java
@@ -1,15 +1,23 @@
 package net.shagie.tasks;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
 public class Sorter {
-    public static void main(String[] args) throws IOException {
-        System.out.println("hello world");
+    private static final Logger LOG = LoggerFactory.getLogger(Sorter.class);
+
+    public static void main(String[] args) {
         Path path = Paths.get("numbers.txt");
-        String read = Files.readAllLines(path).get(0);
-        System.out.println(read);
+        try {
+            String read = Files.readAllLines(path).get(0);
+            System.out.println(read);
+        } catch (IOException e) {
+            LOG.error("Error reading numbers.txt", e);
+        }
     }
 }
