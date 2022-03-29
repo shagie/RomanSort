bfa6023e6886e6a3b7109ea4b150b06ce83b7229
shagie <turnmichael@gmail.com>
2022-03-28 19:48:58 -0500

    feat: Read (and verify that its read) file

--- src/main/java/net/shagie/tasks/Sorter.java
+++ src/main/java/net/shagie/tasks/Sorter.java
@@ -1,7 +1,15 @@
 package net.shagie.tasks;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 public class Sorter {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         System.out.println("hello world");
+        Path path = Paths.get("numbers.txt");
+        String read = Files.readAllLines(path).get(0);
+        System.out.println(read);
     }
 }
