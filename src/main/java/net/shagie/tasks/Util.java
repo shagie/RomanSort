6ae70ff100b8bcb2101e0a37fb3545408de7aa09
shagie <turnmichael@gmail.com>
2022-03-28 20:32:40 -0500

    feat: Initial romanToArabic

---
+++ src/main/java/net/shagie/tasks/Util.java
@@ -0,0 +1,68 @@
+package net.shagie.tasks;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Locale;
+
+public class Util {
+    private static final Logger LOG = LoggerFactory.getLogger(Util.class);
+
+    public static int romanToArabic(String roman) {
+        int state = 1;
+        int sum = 0;
+
+        for (String c : roman.toUpperCase(Locale.ROOT).split("")) {
+
+            sum +=
+                    switch (c) {
+                        case "I" -> ((state < 2) ? 1 : -1);
+                        case "V" -> {
+                            if (state < 3) {
+                                state = 2;
+                            }
+                            yield ((state < 3) ? 5 : -5);
+                        }
+                        case "X" -> {
+                            if (state < 4) {
+                                state = 3;
+                            }
+                            yield ((state < 4) ? 10 : -10);
+                        }
+                        case "L" -> {
+                            if (state < 5) {
+                                state = 4;
+                            }
+                            yield ((state < 5) ? 50 : -50);
+                        }
+                        case "C" -> {
+                            if (state < 6) {
+                                state = 5;
+                            }
+                            yield ((state < 6) ? 100 : -100);
+                        }
+                        case "D" -> {
+                            if (state < 7) {
+                                state = 6;
+                            }
+                            yield ((state < 7) ? 500 : -500);
+                        }
+                        case "M" -> {
+                            state = 7;
+                            yield 1000;
+                        }
+                        default -> {
+                            LOG.error("Invalid character {}", c);
+                            yield 0;
+                        }
+                    };
+        }
+
+        return sum;
+
+    }
+
+    public static String arabicToRoman(int num) {
+        return "";
+    }
+}
